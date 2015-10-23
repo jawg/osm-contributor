f2b9c66d5143d3a5a7c3fcba9f2db3dc1b4d358b
Nicolas Favier <nfavier@excilys.com>
2015-10-23 12:25:32 +0200

    fix drawing limit bounds
    Conflicts:
    	src/main/java/io/mapsquare/osmcontributor/map/vectorial/BoxOverlay.java

--- src/main/java/io/mapsquare/osmcontributor/map/vectorial/BoxOverlay.java
+++ src/main/java/io/mapsquare/osmcontributor/map/vectorial/BoxOverlay.java
@@ -18,88 +18,86 @@
  */
 package io.mapsquare.osmcontributor.map.vectorial;
 
-import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.Rect;
 
-import com.mapbox.mapboxsdk.overlay.Overlay;
+import com.mapbox.mapboxsdk.overlay.SafeDrawOverlay;
 import com.mapbox.mapboxsdk.views.MapView;
+import com.mapbox.mapboxsdk.views.safecanvas.ISafeCanvas;
+import com.mapbox.mapboxsdk.views.safecanvas.SafePaint;
 import com.mapbox.mapboxsdk.views.util.Projection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import io.mapsquare.osmcontributor.utils.Box;
 
-public class BoxOverlay extends Overlay {
+public class BoxOverlay extends SafeDrawOverlay {
 
     private Box box;
     private Clipper clipper;
-    private Paint paint;
-    private List<XY> pointsToClip;
-    private List<XY> clipPoints;
+    private SafePaint paint;
+    private List<XY> clipPoints = new ArrayList<>();
     private double[] projection;
     private Projection pj;
     private double[] projectionResult;
-    private List<XY> points;
-    private Path path = new Path();
+    private XY nw, ne, se, sw;
 
 
     public BoxOverlay(Box box) {
         this.box = box;
         clipper = new Clipper();
-        pointsToClip = new ArrayList<>();
-        points = new ArrayList<>();
-        paint = new Paint();
+        paint = new SafePaint();
 
         paint.setColor(Color.RED);
         paint.setStrokeWidth(8);
         paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeCap(Paint.Cap.ROUND);
 
         projection = Projection.latLongToPixelXY(box.getNorth(), box.getWest());
-        points.add(new XY(projection[0], projection[1]));
+        nw = new XY(projection[0], projection[1]);
+
         projection = Projection.latLongToPixelXY(box.getNorth(), box.getEast());
-        points.add(new XY(projection[0], projection[1]));
-        projection = Projection.latLongToPixelXY(box.getSouth(), box.getEast());
-        points.add(new XY(projection[0], projection[1]));
-        projection = Projection.latLongToPixelXY(box.getSouth(), box.getWest());
-        points.add(new XY(projection[0], projection[1]));
-        projection = Projection.latLongToPixelXY(box.getNorth(), box.getWest());
-        points.add(new XY(projection[0], projection[1]));
+        ne = new XY(projection[0], projection[1]);
 
+        projection = Projection.latLongToPixelXY(box.getSouth(), box.getEast());
+        se = new XY(projection[0], projection[1]);
+
+        projection = Projection.latLongToPixelXY(box.getSouth(), box.getWest());
+        sw = new XY(projection[0], projection[1]);
     }
 
     @Override
-    protected void draw(Canvas canvas, MapView mapView, boolean b) {
+    protected void drawSafe(ISafeCanvas canvas, MapView mapView, boolean b) {
         if (b) {
-            pointsToClip.clear();
-            path.reset();
-
             pj = mapView.getProjection();
             clipper.setClippingBounds(enlargeRect(pj.getScreenRect(), 100));
 
-            projectionResult = new double[2];
+            drawClipLine(nw, ne, canvas);
+            drawClipLine(ne, se, canvas);
+            drawClipLine(se, sw, canvas);
+            drawClipLine(sw, nw, canvas);
+        }
+    }
 
-            for (XY point : points) {
-                pj.toMapPixelsTranslated(point.getAsDoubleArray(), projectionResult);
-                pointsToClip.add(new XY(projectionResult[0], projectionResult[1]));
-            }
+    private void drawClipLine(XY a, XY b, ISafeCanvas canvas) {
+        XY ap, bp;
+        projectionResult = new double[2];
 
-            clipPoints = clipper.clip(pointsToClip, true);
+        pj.toMapPixelsTranslated(a.getAsDoubleArray(), projectionResult);
+        ap = new XY(projectionResult[0], projectionResult[1]);
 
-            if (clipPoints != null && clipPoints.size() > 0) {
-                path.moveTo((float) clipPoints.get(0).getX(), (float) clipPoints.get(0).getY());
-            }
+        pj.toMapPixelsTranslated(b.getAsDoubleArray(), projectionResult);
+        bp = new XY(projectionResult[0], projectionResult[1]);
 
-            // we moved to the first point so we can skip the first element
-            for (int i = 1; i < clipPoints.size(); i++) {
-                path.lineTo((float) clipPoints.get(i).getX(), (float) clipPoints.get(i).getY());
-            }
+        clipPoints.clear();
+        clipPoints = clipper.clip(Arrays.asList(ap, bp), false);
 
+        if (clipPoints.size() == 2) {
             // Draw path
-            canvas.drawPath(path, paint);
+            canvas.drawLine(clipPoints.get(0).getX(), clipPoints.get(0).getY(), clipPoints.get(1).getX(), clipPoints.get(1).getY(), paint);
         }
     }
 
