b7ea7bf685530f879ae696b98032a9ef86a18505
Tommy Buonomo <tommy.buonomo@gmail.com>
2016-05-30 17:05:47 +0200

    Change design of SplashScreen

--- src/main/java/io/mapsquare/osmcontributor/map/MapFragment.java
+++ src/main/java/io/mapsquare/osmcontributor/map/MapFragment.java
@@ -35,7 +35,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
