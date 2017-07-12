0beb3073c1e62dfd0541bcc5f8b8d9c1251cea7c
Mouaad Aallam <maallam@e-biz.fr>
2017-07-12 15:15:20 +0200

    [FEATURE] Show invalidity message if the tag is not conform

--- src/main/java/io/jawg/osmcontributor/ui/adapters/TagsAdapter.java
+++ src/main/java/io/jawg/osmcontributor/ui/adapters/TagsAdapter.java
@@ -25,6 +25,7 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
