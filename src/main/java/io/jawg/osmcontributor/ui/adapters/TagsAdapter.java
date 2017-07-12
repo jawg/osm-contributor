57e1abe28cc75ed751c42fb90f42b84c705e4c7e
Mouaad Aallam <Aallam@users.noreply.github.com>
2017-07-12 16:23:17 +0200

    [CLEANUP] Remove unused import

--- src/main/java/io/jawg/osmcontributor/ui/adapters/TagsAdapter.java
+++ src/main/java/io/jawg/osmcontributor/ui/adapters/TagsAdapter.java
@@ -25,7 +25,6 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
