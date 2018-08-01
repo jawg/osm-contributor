b83ecdb1e8f3a1d20989fd7c8cf75532cbcb8c6b
Sebastien Guillerm <sguillerm@e-biz.fr>
2018-08-01 11:44:21 +0200

    [Feature] WIP - Adding dialog to display and choose bus lines to add to a POI

--- src/main/java/io/jawg/osmcontributor/ui/adapters/binding/BusLinesViewBinder.java
+++ src/main/java/io/jawg/osmcontributor/ui/adapters/binding/BusLinesViewBinder.java
@@ -1,6 +1,7 @@
 package io.jawg.osmcontributor.ui.adapters.binding;
 
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,27 +9,28 @@ import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AutoCompleteTextView;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import io.jawg.osmcontributor.OsmTemplateApplication;
 import io.jawg.osmcontributor.R;
-import io.jawg.osmcontributor.model.entities.RelationId;
+import io.jawg.osmcontributor.model.entities.Poi;
 import io.jawg.osmcontributor.model.entities.relation_display.RelationDisplay;
 import io.jawg.osmcontributor.model.entities.relation_save.RelationEdition;
 import io.jawg.osmcontributor.ui.adapters.BusLineAdapter;
-import io.jawg.osmcontributor.ui.adapters.BusLineSuggestionAdapter;
 import io.jawg.osmcontributor.ui.adapters.item.shelter.TagItem;
 import io.jawg.osmcontributor.ui.adapters.parser.BusLineRelationDisplayParser;
 import io.jawg.osmcontributor.ui.adapters.parser.BusLineValueParserImpl;
 import io.jawg.osmcontributor.ui.adapters.parser.ParserManager;
+import io.jawg.osmcontributor.ui.dialogs.AddBusLinePoiDialogFragment;
 import io.jawg.osmcontributor.ui.managers.RelationManager;
 import io.jawg.osmcontributor.ui.utils.views.DividerItemDecoration;
 import io.jawg.osmcontributor.ui.utils.views.holders.TagItemBusLineViewHolder;
+import io.jawg.osmcontributor.utils.edition.RelationDisplayDto;
 
 public class BusLinesViewBinder extends CheckedTagViewBinder<TagItemBusLineViewHolder, TagItem> {
 
@@ -41,12 +43,14 @@ public class BusLinesViewBinder extends CheckedTagViewBinder<TagItemBusLineViewH
     @Inject
     RelationManager relationManager;
 
-    private AutoCompleteTextView modelText;
-    private Collection<RelationId> backendRelationIds;
+    private static final int NB_RELATIONS_TO_DISPLAY = 3;
+    private static final String TAG_REF = "ref";
 
-    public BusLinesViewBinder(Activity activity, TagItemChangeListener tagItemChangeListener, Collection<RelationId> relationIds) {
+    private Poi poi;
+
+    public BusLinesViewBinder(Activity activity, TagItemChangeListener tagItemChangeListener, Poi poi) {
         super(activity, tagItemChangeListener);
-        this.backendRelationIds = relationIds;
+        this.poi = poi;
         ((OsmTemplateApplication) activity.getApplication()).getOsmTemplateComponent().inject(this);
     }
 
@@ -63,7 +67,7 @@ public class BusLinesViewBinder extends CheckedTagViewBinder<TagItemBusLineViewH
         holder.getTextViewKey().setText(ParserManager.parseTagName(tagItem.getKey(), holder.getContent().getContext()));
 
         //todo don't call bdd on UI thread
-        final List<RelationDisplay> busLines = relationManager.getRelationDisplaysFromRelationsIDs(backendRelationIds);
+        final List<RelationDisplay> busLines = relationManager.getRelationDisplaysFromRelationsIDs(poi.getRelationIds());
 
         BusLineAdapter adapter = new BusLineAdapter(busLines, busLineRelationDisplayParser);
 
@@ -71,32 +75,62 @@ public class BusLinesViewBinder extends CheckedTagViewBinder<TagItemBusLineViewH
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(activity.get()));
         recyclerView.setHasFixedSize(false);
+
         RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
         itemAnimator.setAddDuration(300);
         recyclerView.setItemAnimator(itemAnimator);
         recyclerView.addItemDecoration(new DividerItemDecoration(activity.get()));
-        adapter.setRemoveBusListener((lineRemoved) -> {
-            onRelationItemChange(new Pair<>(lineRemoved, RelationEdition.RelationModificationType.REMOVE_MEMBER));
-        });
 
-      /*  holder.getEditAddButton().setOnClickListener(
+        adapter.setRemoveBusListener((lineRemoved) -> onRelationItemChange(new Pair<>(lineRemoved, RelationEdition.RelationModificationType.REMOVE_MEMBER)));
+
+        // TODO don't call bdd on UI thread
+        final List<RelationDisplay> nearestBusLines = relationManager.getBusLinesOrderedByDistanceFromPoiById(poi.getId());
+
+        HashSet<RelationDisplay> relationDisplaySet = new HashSet<>(busLines);
+
+        holder.getEditAddButton().setOnClickListener(
                 view -> {
-                    String lineValue = holder.getTextViewValue().getText().toString();
-                    if (!lineValue.trim().equals("")) {
-                        addBusLine(tagItem, busLines, adapter, lineValue);
+                    List<RelationDisplay> nearestBusLinesFiltered = new ArrayList<>();
+
+                    int nbBusLines = 0;
+                    boolean isBusLineToAdd;
+                    for (RelationDisplay nearestBusLine : nearestBusLines) {
+
+                        // Check if the busLine is already in relations
+                        if (isBusLineToAdd = !relationDisplaySet.contains(nearestBusLine)) {
+                            RelationDisplayDto nearestBusLineDto = new RelationDisplayDto(nearestBusLine);
+
+                            for (RelationDisplay busLine : busLines) {
+                                RelationDisplayDto busLineDto = new RelationDisplayDto(busLine);
+
+                                // Check if the busLine 'ref' tag corresponding to an existing busline ref in relations
+                                if (nearestBusLineDto.getTag(TAG_REF).equals(busLineDto.getTag(TAG_REF))) {
+                                    isBusLineToAdd = false;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (isBusLineToAdd) {
+                            nearestBusLinesFiltered.add(nearestBusLine);
+                            nbBusLines++;
+
+                            if (nbBusLines == NB_RELATIONS_TO_DISPLAY) {
+                                break;
+                            }
+                        }
                     }
-                    holder.getTextViewValue().getText().clear();
-                });*/
 
-
-        BusLineSuggestionAdapter suggestionAdapter = new BusLineSuggestionAdapter(activity.get().getApplicationContext(), relationManager, busLineRelationDisplayParser, busLines);
-        modelText = holder.getTextViewValue();
-        modelText.setAdapter(suggestionAdapter);
-        modelText.setOnItemClickListener((parent, view, position, id) -> {
-            addBusLine(busLines, adapter, new Pair<>(suggestionAdapter.getItem(position), RelationEdition.RelationModificationType.ADD_MEMBER));
-            holder.getTextViewValue().getText().clear();
-        });
-        //  modelText.setDropDownHeight((int) (120 * activity.get().getApplication().getResources().getDisplayMetrics().density));
+                    AddBusLinePoiDialogFragment.display(
+                            ((AppCompatActivity) activity.get()).getSupportFragmentManager(),
+                            relationManager,
+                            busLineRelationDisplayParser,
+                            poi,
+                            busLines,
+                            nearestBusLinesFiltered,
+                            (busLine) -> addBusLine(busLines, adapter, new Pair<>(busLine, RelationEdition.RelationModificationType.ADD_MEMBER)));
+                }
+        );
     }
 
     private void addBusLine(List<RelationDisplay> busLines, BusLineAdapter adapter, Pair<RelationDisplay, RelationEdition.RelationModificationType> change) {
---
+++ src/main/java/io/jawg/osmcontributor/utils/edition/RelationDisplayDto.java
@@ -0,0 +1,61 @@
+package io.jawg.osmcontributor.utils.edition;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import io.jawg.osmcontributor.model.entities.relation_display.RelationDisplay;
+import io.jawg.osmcontributor.model.entities.relation_display.RelationDisplayTag;
+
+public class RelationDisplayDto {
+
+    private Long id;
+    private String backendId;
+    private HashMap<String, String> tags;
+
+    public RelationDisplayDto(RelationDisplay relationDisplay) {
+
+        this.id = relationDisplay.getId();
+        this.backendId = relationDisplay.getBackendId();
+        this.tags = tagsMapper(relationDisplay.getTags());
+    }
+
+    private HashMap<String,String> tagsMapper(Collection<RelationDisplayTag> tagsList) {
+        HashMap<String, String> tagsMap = new HashMap<>(tagsList.size());
+        for (RelationDisplayTag tag : tagsList) {
+            tagsMap.put(tag.getKey(), tag.getValue());
+        }
+        return tagsMap;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBackendId() {
+        return this.backendId;
+    }
+
+    public void setBackendId(String backendId) {
+        this.backendId = backendId;
+    }
+
+    public HashMap<String, String> getTags() {
+        return tags;
+    }
+
+    public void setTags(HashMap<String, String> tags) {
+        this.tags = tags;
+    }
+
+    public String getTag(String key) {
+        return tags.get(key);
+    }
+
+    public void setTag(String key, String value) {
+        this.tags.put(key, value);
+    }
+}
